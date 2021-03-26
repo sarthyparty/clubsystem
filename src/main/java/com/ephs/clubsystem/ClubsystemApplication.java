fd8ca6e069e2f2da6af5f297370133941b56f8a9
Sarthak Agrawal <sarthavader@gmail.com>
2021-03-26 13:34:17 -0500

    fixed everything

--- src/main/java/com/ephs/clubsystem/ClubsystemApplication.java
+++ src/main/java/com/ephs/clubsystem/ClubsystemApplication.java
@@ -2,9 +2,6 @@ package com.ephs.clubsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @SpringBootApplication
 public class ClubsystemApplication {
