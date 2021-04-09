d8527f379a15eb382a42f6946eaefc2e7da7985d
Sai Guntabonu <saiguntabonu@gmail.com>
2021-04-08 21:47:52 -0500

    added event tables

--- src/main/java/com/ephs/clubsystem/model/Event.java
+++ src/main/java/com/ephs/clubsystem/model/Event.java
@@ -12,6 +12,31 @@ public class Event {
     private String startdate;
     private String enddate;
     private String club;
+    public long getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) { this.description = description; }
+
+    public String getEnddate() {
+        return enddate;
+    }
+    public void setEnddate(String enddate) { this.enddate= enddate; }
+    public String getStartdate() { return startdate; }
+    public void setStartdate(String startdate) {
+        this.startdate= startdate;
+    }
     @ManyToMany
             @JoinTable(
                     name= "UserEventRelationship",
@@ -19,7 +44,9 @@ public class Event {
                     inverseJoinColumns = @JoinColumn(name = "eventID"))
 
     Set <User> likes;
-
+    @ManyToOne
+    @JoinColumn(name="club_id")
+    private Club club_;
 
 
 }
